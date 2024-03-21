88efc0888006483f1b1671fef1778c1764409830
MarkijanGuljak <164378051+MarkijanGuljak@users.noreply.github.com>
2024-03-21 18:25:48 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const btns = document.getElementsByTagName('button'),
+    wrapper = document.getElementsByClassName('wrapper'),
+    x = document.getElementsByTagName('span')[0];
+function load() {
+    wrapper.style.display = "none"
+}
+load();
+btns[0].addEventListener('click',()=>{
+    wrapper.style.display = "block";
+})
+
+x.addEventListener('click',()=>{
+    wrapper.style.display = "block";
+})
